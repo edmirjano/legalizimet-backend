6b0a69aead77dc597c354d784263b6f030cdf907
Edmirjano <edmirjano@icloud.com>
2021-07-05 01:31:06 +0200

    first commtoto

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.get('/', (req, res) => {
   })
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
